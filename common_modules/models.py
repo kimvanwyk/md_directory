42f49ccc679d8a9810fe35d04dc67fe0dc270452
Kim van Wyk <kimv@sahomeloans.com>
2019-03-09 17:00:01 +0200

    Remove superfluous struct

--- common_modules/models.py
+++ common_modules/models.py
@@ -81,28 +81,6 @@ class Struct(models.Model):
         n = '%s %s%s' % (self.type.type, i, self.name)            
         return n
 
-class Struct(models.Model):
-    name = models.CharField('Structure Name', max_length=100)    
-    parent = models.ForeignKey('self', null=True, blank=True, 
-                               help_text = 'Set to name of parent Multiple District. Leave blank if this is already a Multiple District')
-    website = models.URLField(blank=True)
-    type = models.ForeignKey(StructType, verbose_name="Type of district", null=True, blank=True)
-
-    class Meta:
-        app_label = 'md_directory'
-        ordering = ['id']
-
-    def __unicode__(self):
-        if self.parent_id:
-            try:
-                i = self.parent.name
-            except:
-                i = ''
-        else:
-            i = ''
-        n = '%s %s%s' % (self.type.type, i, self.name)            
-        return n
-
 class Region(models.Model):
     ''' Represent a region
     '''
