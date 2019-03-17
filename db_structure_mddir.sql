0944cd42cce258b1eddc45c23687a5ca5e5a7f32
Kim van Wyk <kimv@sahomeloans.com>
2019-03-17 17:04:31 +0200

    PDG Eddie

--- db_structure_mddir.sql
+++ db_structure_mddir.sql
@@ -306,7 +306,7 @@ CREATE TABLE `md_directory_clubmerge` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `club_id` (`club_id`),
   UNIQUE KEY `club_id_2` (`club_id`,`new_struct_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=381 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=350 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -789,4 +789,4 @@ CREATE TABLE `south_migrationhistory` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-11  8:22:43
+-- Dump completed on 2019-03-17 15:04:31
