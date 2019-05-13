93fdf41f645fe2ffdac4d32efcad9f56105140e7
Kim van Wyk <kimv@sahomeloans.com>
2019-05-13 15:52:26 +0200

    Move helper function higher in class

--- common_modules/db_handler.py
+++ common_modules/db_handler.py
@@ -158,6 +158,59 @@ class DBHandler(object):
         res = db.conn.execute(t.select(t.c.struct_id == struct_id).order_by(getattr(t.c, order_field))).fetchall()
         return [getter(r.id) for r in res]
 
+    def get_title(self, member_id, 
+                  struct_officers = ((19, 0, operator.eq, "IP"),  (19, -1, operator.eq, "PIP"),  (21, 0, operator.eq, "ID"),  
+                                     (21, -1, operator.eq, "ID"),  (11, 0, operator.eq, "CC"),  (5, 0, operator.eq, "DG"),  
+                                     (11, -1, operator.le, "PCC"),  (13, 0, operator.eq, "CCE"),  (5, -1, operator.eq, "IPDG"),  
+                                     (7, 0, operator.eq, "1VDG"),  (8, 0, operator.eq, "2VDG"),  (5, -2, operator.le, "PDG"),  
+                                     (14, 0, operator.eq, "CS"),  (15, 0, operator.eq, "CT"),  (9, 0, operator.eq, "DCS"),  
+                                     (10, 0, operator.eq, "DCT")),
+                  club_officers = ((1, 0, operator.eq, "LP"), (2, 0, operator.eq, "LS"), 
+                                   (3, 0, operator.eq, "LT"), (1, -1, operator.le, "PLP"))):
+        ''' Return a title for the supplied member_id, or None
+        if none found
+        '''
+        def search_officers(member_id, table, mapping):
+            title = None
+            t = self.tables[table]
+            res = db.conn.execute(t.select(t.c.member_id == member_id)).fetchall()
+            index = 100
+            for (n,(office_id, addition, op, ttl)) in enumerate(mapping):
+                for r in res:
+                    if all(((office_id == r.office_id), (op(r.year, self.year + addition)),
+                            (n < index))):
+                        index = n
+                        title = ttl
+                        break
+            return title
+
+        title = search_officers(member_id, 'structofficer', struct_officers)
+        if not title:
+            for (table, ttl) in (('regionchair', 'RC'), ('zonechair', 'ZC')):
+                t = self.tables[table]
+                res = db.conn.execute(t.select(and_(t.c.member_id == member_id,
+                                                    t.c.year == self.year))).fetchall()
+                if res:
+                    title = ttl
+                    break
+        if not title:
+            for (type_id, ttl) in ((1, 'MDC'), (0, 'DC')):
+                t = self.tables['struct']
+                structs = db.conn.execute(t.select(t.c.type_id == type_id)).fetchall()
+                t = self.tables['structchair']
+                for struct in structs:
+                    res = db.conn.execute(t.select(and_(t.c.member_id == member_id,
+                                                        t.c.year == self.year,
+                                                        t.c.struct_id == struct.id))).fetchall()
+                    if res:
+                        title = ttl
+                        break
+                if title:
+                    break
+        if not title:
+            title = search_officers(member_id, 'clubofficer', club_officers)
+        return title
+
     def get_member(self, member_id, mapping={'deceased_b': 'is_deceased', 'resigned_b': 'is_resigned',
                                              'partner_lion_b': 'is_partner_lion', 'club_id': 'club'},
                    email=None):
@@ -302,59 +355,6 @@ class DBHandler(object):
     def get_past_dgs(self, struct_id):
         return self.get_past_struct_officers(struct_id, 5)
 
-    def get_title(self, member_id, 
-                  struct_officers = ((19, 0, operator.eq, "IP"),  (19, -1, operator.eq, "PIP"),  (21, 0, operator.eq, "ID"),  
-                                     (21, -1, operator.eq, "ID"),  (11, 0, operator.eq, "CC"),  (5, 0, operator.eq, "DG"),  
-                                     (11, -1, operator.le, "PCC"),  (13, 0, operator.eq, "CCE"),  (5, -1, operator.eq, "IPDG"),  
-                                     (7, 0, operator.eq, "1VDG"),  (8, 0, operator.eq, "2VDG"),  (5, -2, operator.le, "PDG"),  
-                                     (14, 0, operator.eq, "CS"),  (15, 0, operator.eq, "CT"),  (9, 0, operator.eq, "DCS"),  
-                                     (10, 0, operator.eq, "DCT")),
-                  club_officers = ((1, 0, operator.eq, "LP"), (2, 0, operator.eq, "LS"), 
-                                   (3, 0, operator.eq, "LT"), (1, -1, operator.le, "PLP"))):
-        ''' Return a title for the supplied member_id, or None
-        if none found
-        '''
-        def search_officers(member_id, table, mapping):
-            title = None
-            t = self.tables[table]
-            res = db.conn.execute(t.select(t.c.member_id == member_id)).fetchall()
-            index = 100
-            for (n,(office_id, addition, op, ttl)) in enumerate(mapping):
-                for r in res:
-                    if all(((office_id == r.office_id), (op(r.year, self.year + addition)),
-                            (n < index))):
-                        index = n
-                        title = ttl
-                        break
-            return title
-
-        title = search_officers(member_id, 'structofficer', struct_officers)
-        if not title:
-            for (table, ttl) in (('regionchair', 'RC'), ('zonechair', 'ZC')):
-                t = self.tables[table]
-                res = db.conn.execute(t.select(and_(t.c.member_id == member_id,
-                                                    t.c.year == self.year))).fetchall()
-                if res:
-                    title = ttl
-                    break
-        if not title:
-            for (type_id, ttl) in ((1, 'MDC'), (0, 'DC')):
-                t = self.tables['struct']
-                structs = db.conn.execute(t.select(t.c.type_id == type_id)).fetchall()
-                t = self.tables['structchair']
-                for struct in structs:
-                    res = db.conn.execute(t.select(and_(t.c.member_id == member_id,
-                                                        t.c.year == self.year,
-                                                        t.c.struct_id == struct.id))).fetchall()
-                    if res:
-                        title = ttl
-                        break
-                if title:
-                    break
-        if not title:
-            title = search_officers(member_id, 'clubofficer', club_officers)
-        return title
-
 def get_db_settings(fn='db_settings.ini', sec='DB'):
     settings = {}
     cp = ConfigParser.SafeConfigParser()
@@ -383,9 +383,9 @@ db = DBHandler(year=2019, **get_db_settings())
 # pprint(db.get_region_zones(4))
 # pprint(db.get_zone_clubs(41))
 
-pprint([(c.name, (c.zone.name, c.zone.region.name) if c.zone else 'No Zone') for c in db.get_district_clubs(9) if not c.is_closed])
-pprint([r.name for r in db.get_district_regions(9)])
-pprint([z.name for z in db.get_district_zones(9)])
+# pprint([(c.name, (c.zone.name, c.zone.region.name) if c.zone else 'No Zone') for c in db.get_district_clubs(9) if not c.is_closed])
+# pprint([r.name for r in db.get_district_regions(9)])
+# pprint([z.name for z in db.get_district_zones(9)])
 
 # pprint([(po.year, po.end_month, po.member.first_name, po.member.last_name) for po in db.get_past_ccs(5)])
 # pprint([(po.year, po.end_month, po.previous_district, po.member.first_name, po.member.last_name) for po in db.get_past_dgs(9)])
