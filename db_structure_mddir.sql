81ef2de9cb6ec52cea2e86bf797f4d38c6186167
Kim van Wyk <kimv@sahomeloans.com>
2019-07-18 12:30:25 +0200

    Several data changes

--- db_structure_mddir.sql
+++ db_structure_mddir.sql
@@ -335,7 +335,7 @@ CREATE TABLE `md_directory_clubofficer` (
   KEY `mddir_clubofficer_1985cacc` (`club_id`),
   KEY `mddir_clubofficer_56e38b98` (`member_id`),
   KEY `mddir_clubofficer_43edbc56` (`office_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10551 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10552 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -789,4 +789,4 @@ CREATE TABLE `south_migrationhistory` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-09  9:05:41
+-- Dump completed on 2019-07-18 10:30:25
