c60ae8a35e4d30673a4a80bb32a68dfc8651a813
Kim van Wyk <kimv@sahomeloans.com>
2020-07-10 11:27:47 +0200

    Club feedback

--- db_structure_mddir.sql
+++ db_structure_mddir.sql
@@ -335,7 +335,7 @@ CREATE TABLE `md_directory_clubofficer` (
   KEY `mddir_clubofficer_1985cacc` (`club_id`),
   KEY `mddir_clubofficer_56e38b98` (`member_id`),
   KEY `mddir_clubofficer_43edbc56` (`office_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11009 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11018 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -352,7 +352,7 @@ CREATE TABLE `md_directory_clubofficer_email` (
   `office_id` smallint(6) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `club_id` (`club_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -687,7 +687,7 @@ CREATE TABLE `md_directory_structchair` (
   PRIMARY KEY (`id`),
   KEY `mddir_structchair_408cafa9` (`struct_id`),
   KEY `mddir_structchair_56e38b98` (`member_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1078 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1079 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -806,4 +806,4 @@ CREATE TABLE `south_migrationhistory` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-07 10:29:43
+-- Dump completed on 2020-07-10  9:27:47
