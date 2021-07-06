9314362343a99ef99cf489ad72c3af0017a20eff
Kim van Wyk <kimv@sahomeloans.com>
2021-07-06 09:20:38 +0200

    Tokai website, member email

--- db_structure_mddir.sql
+++ db_structure_mddir.sql
@@ -806,4 +806,4 @@ CREATE TABLE `south_migrationhistory` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-05  9:36:20
+-- Dump completed on 2021-07-06  7:20:38
