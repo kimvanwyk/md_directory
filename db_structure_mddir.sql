f4f1ddf2dd1d9b3525d4fad06cc4be4c3eb7f614
kimv <kimv@sahomeloans.com>
2023-04-01 17:04:25 +0200

    recent changes

--- db_structure_mddir.sql
+++ db_structure_mddir.sql
@@ -355,7 +355,7 @@ CREATE TABLE `md_directory_clubofficer` (
   KEY `mddir_clubofficer_1985cacc` (`club_id`),
   KEY `mddir_clubofficer_56e38b98` (`member_id`),
   KEY `mddir_clubofficer_43edbc56` (`office_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16816 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16819 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -850,4 +850,4 @@ USE `md_directory`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-20  8:22:10
+-- Dump completed on 2023-04-01 15:04:25
